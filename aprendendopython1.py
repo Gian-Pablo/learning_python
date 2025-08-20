04af08c23da86defd28a0fa1ea5cfd0f590c70da
Gian Pablo <gian.torres@etec.sp.gov.br>
2025-08-20 12:59:54 -0300

    primeira aula de python

---
+++ aprendendopython1.py
@@ -0,0 +1,15 @@
+# Definindo variáveis
+nome = "Gian Pablo"
+idade = 17
+altura = 1.79
+situacaoTrabalho = False
+
+# Exibindo os dados com a função print
+
+print('Hello World!')
+print('A soma é: ',2+3)
+print('A subtração é: ', 6-5)
+print('A multiplicação é: {}'.format(5*5)) # 2º formato mais ideal
+print(f'A divisão é: {10/2}') # 1º formato mais ideal
+
+print(f'Seu nome é {nome}, sua idade é {idade} anos, sua altura é {altura} metros, sua situação de trabalho é {situacaoTrabalho}') # mais ideal para textos longos.
